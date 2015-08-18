d3a6ebd8cde2771dbc23b9593e0b72c157ee2520
Langes <langes72@gmail.com>
2015-08-18 18:36:42 +0100

    use twrp as default recovery
    Change-Id: Ide0e0004958cfb0f9f42ac75dea94292421ef52a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# we need this set before including the other makefiles
+RECOVERY_VARIANT := twrp
+
 # inherit from the common rhine definitions
 include device/sony/rhine-common/BoardConfigCommon.mk
 
